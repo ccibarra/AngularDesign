81db4f6853e1570125511e08d0c646e7706e08da
christian ibarra <christian.ibarra@neverbit.com>
2021-11-28 19:54:29 -0500

    porble en el ngfor solucionado

--- src/app/information-board/information-board.component.ts
+++ src/app/information-board/information-board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup} from '@angular/forms';
+import { shareReplay, subscribeOn } from 'rxjs/operators';
 import Swal from 'sweetalert2';
 import { ApiService } from '../metod/api.service';
 import {RecordModel} from './information-dash board.model';
@@ -48,6 +49,43 @@ export class InformationBoardComponent implements OnInit {
     .subscribe (res=>{
       this.recordData = res.usuarios;
     })
+  }
+
+  deleteRecord(row:any){
+    this.api.deleteRegistration(row.uid)
+    .subscribe(res=>{
+      Swal.fire ('Eliminado con Exito')
+      this.getRecord();
+    },
+    err=>{
+      Swal.fire('No hay token para validar')
+    }
+    )
+
+  }
+
+  onEdit(row: any){
+    this.RecordsModelObj.id =row.id;
+    this.formValue.controls ['nombre'].setValue(row.nombre);
+    this.formValue.controls ['correo'].setValue(row.correo);
+    this.formValue.controls ['rol'].setValue(row.rol);
+  }
+  updateRecord(){
+    this.RecordsModelObj.nombre = this. formValue.value.nombre;
+    this.RecordsModelObj.correo =this.formValue.value.correo;
+    this.RecordsModelObj.rol = this.formValue.value.rol;
+    this.RecordsModelObj.password = this.formValue.value.password ;
+
+    this.api.updateRegistration(this.RecordsModelObj,this.RecordsModelObj.id)
+    .subscribe(res=>{
+      Swal.fire ('Actualizado con Exito')
+      this.getRecord();
+    },
+    err=>{
+      Swal.fire('Algo salio mal')
+    }
+    )
+
   }
   
   
